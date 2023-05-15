d5eb06ff6f8f831fa404a6d0c42fcc0df91560b8
Alexander Levterov <shuroffrashiday@yandex.ru>
2023-05-15 17:26:57 +0300

    тестовая версия кейсов готова

--- src/components/sections/CasesSection/CasesSection.tsx
+++ src/components/sections/CasesSection/CasesSection.tsx
@@ -2,14 +2,41 @@ import BlackTitle from '@/components/ui/Common/BlackTitle/BlackTitle'
 import styles from './CasesSection.module.css'
 import global from '@/styles/global.module.css'
 import DraggableLine from '@/components/ui/Sections/CasesSection/DraggableLine/DraggableLine'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import CasesItem from '@/components/ui/Sections/CasesSection/CasesItem/CasesItem'
-import { itemsTop } from './CasesItems'
+import { itemsBottom, itemsTop } from './CasesItems'
+import {
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+  motion,
+} from 'framer-motion'
+
+function isNumber(number: number | null): number {
+  if (number === null) {
+    return 0
+  } else {
+    return number
+  }
+}
+
 const CasesSection = () => {
-  const [offsetX, setOffsetX] = useState<null | number>(null)
+  const [offsetX, setOffsetX] = useState<null | number>(0)
+  const physics = { damping: 15, mass: 0.27, stiffness: 55 }
+  const spring = useSpring(isNumber(offsetX), physics)
   useEffect(() => {
-    console.log(offsetX)
-  }, [offsetX])
+    spring.set(isNumber(offsetX) * -6)
+  }, [offsetX, spring])
+  const ref = useRef(null)
+  const { scrollXProgress } = useScroll({ container: ref })
+
+  console.log('spring', spring)
+  console.log('offsetX', offsetX)
+
+  useEffect(() => {
+    console.log('scrollXProgress: ', scrollXProgress)
+  }, [scrollXProgress])
   return (
     <div className={styles.section}>
       <div className={global.container}>
@@ -17,7 +44,11 @@ const CasesSection = () => {
           <BlackTitle color='black'>Кейсы</BlackTitle>
           <DraggableLine offsetX={offsetX} setOffsetX={setOffsetX} />
         </div>
-        <div className={styles.scrollableBlock}>
+        <motion.section
+          className={styles.scrollableBlock}
+          ref={ref}
+          style={{ x: spring }}
+        >
           <div className={styles.cardsTopContainer}>
             {itemsTop.map(item => (
               <CasesItem
@@ -29,7 +60,17 @@ const CasesSection = () => {
             ))}
           </div>
           <div className={styles.mainLine}></div>
-        </div>
+          <div className={styles.cardsBottomContainer}>
+            {itemsBottom.map(item => (
+              <CasesItem
+                text={item.text}
+                year={item.year}
+                key={item.text}
+                position='bottom'
+              />
+            ))}
+          </div>
+        </motion.section>
       </div>
     </div>
   )
